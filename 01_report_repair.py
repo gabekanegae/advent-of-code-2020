a62caecdf0ab9375353733f900e1d6c425a9a4e1
Gabriel Kanegae <gabekanegae@gmail.com>
2020-12-01 02:27:22 -0300

    Day 1! ⭐⭐

---
+++ 01_report_repair.py
@@ -0,0 +1,34 @@
+################################
+# --- Day 1: Report Repair --- #
+################################
+
+import AOCUtils
+
+def twoSum(counts, target):
+    for i, ct in counts.items():
+        if target-i in counts:
+            return i * (target-i)
+
+def threeSum(counts, target):
+    for i in counts.keys():
+        twoSumResult = twoSum(counts, target-i)
+        if twoSumResult is not None:
+            return i * twoSumResult
+
+################################
+
+report = AOCUtils.loadInput(1)
+
+target = 2020
+
+reportCounts = dict()
+for i in report:
+    if i not in reportCounts:
+        reportCounts[i] = 0
+    reportCounts[i] += 1
+
+print("Part 1: {}".format(twoSum(reportCounts, target)))
+
+print("Part 2: {}".format(threeSum(reportCounts, target)))
+
+AOCUtils.printTimeTaken()
