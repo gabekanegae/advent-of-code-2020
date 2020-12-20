6d35aabcfbfd29289cfa2efa71a1c2942f6d1004
Gabriel Kanegae <gabekanegae@gmail.com>
2020-12-20 09:00:21 -0300

    Day 20 - Code cleanup

--- 20_jurassic_jigsaw.py
+++ 20_jurassic_jigsaw.py
@@ -12,11 +12,7 @@ class Tile:
 
     @property
     def imageWithoutBorders(self):
-        self._imageWithoutBorders = []
-        for line in self.image[1:-1]:
-            self._imageWithoutBorders.append(line[1:-1])
-
-        return self._imageWithoutBorders
+        return [row[1:-1] for row in self.image[1:-1]]
     
     @property
     def sides(self):
@@ -25,13 +21,7 @@ class Tile:
         left = [self.image[i][0] for i in range(len(self.image[0]))]
         right = [self.image[i][-1] for i in range(len(self.image[0]))]
 
-        up = "".join(up)
-        left = "".join(left)
-        down = "".join(down)
-        right = "".join(right)
-
-        self._sides = {"U": up, "D": down, "L": left, "R": right}
-        return self._sides
+        return {"U": up, "D": down, "L": left, "R": right}
 
 def rot90CW(matrix):
     return [list(l) for l in zip(*matrix[::-1])]
@@ -70,12 +60,12 @@ for tile1ID, tile1 in tiles.items():
 
         for side1Direction, side1 in tile1.sides.items():
             for side2Direction, side2 in tile2.sides.items():
-                if side1 in [side2, "".join(reversed(side2))]:
+                if side1 in [side2, side2[::-1]]:
                     if tile1ID not in tileConnections:
                         tileConnections[tile1ID] = dict()
                     tileConnections[tile1ID][side1Direction] = tile2ID
 
-# Assume there will be only one possible match/choice/placement for each piece
+# Assume there will be only one possible match/choice/placement for each tile
 cornerTiles = [tileID for tileID, conns in tileConnections.items() if len(conns) == 2]
 
 p1 = 1
@@ -88,16 +78,16 @@ mov4 = {"R": (0, 1), "D": (1, 0), "L": (0, -1), "U": (-1, 0)}
 
 dirsOpposite = {"D": "U", "U": "D", "L": "R", "R": "L"}
 dirsRot90CW = {"D": "L", "U": "R", "L": "U", "R": "D"}
-dirsFlipH = {k: v if k in "LR" else dirsOpposite[v] for k, v in dirsOpposite.items()}
+dirsFlipH = {k: (v if k in "LR" else dirsOpposite[v]) for k, v in dirsOpposite.items()}
 
 # Figure out the correct tile placement (after tiles are
 # rotated/flipped), building a `grid` matrix made of tile IDs.
-# Assume grid is square (i.e. len(tiles) is a perfect square)
-gridSize = int(len(tiles)**0.5)
+# Assume grid is square (i.e. `len(tiles)` is a perfect square)
+gridSize = int(len(tiles) ** 0.5)
 grid = [[None for _ in range(gridSize)] for _ in range(gridSize)]
 
-# Pick any corner tile (doesn't matter which, as
-# long as startPos is set based on their neighbors)
+# Pick any corner tile (doesn't matter which, as long
+# as `startPos` is set based on their neighbors)
 startTile = cornerTiles[0]
 if all(c in tileConnections[startTile] for c in "DR"): # Top-left
     startPos = (0, 0)
@@ -108,6 +98,7 @@ elif all(c in tileConnections[startTile] for c in "UR"): # Bottom-left
 elif all(c in tileConnections[startTile] for c in "UL"): # Bottom-right
     startPos = (len(grid)-1, len(grid)-1)
 
+# BFS starting from the picked corner until the grid is filled
 queue = deque([(startPos, startTile)])
 tilesPlaced = set()
 while queue:
@@ -120,7 +111,7 @@ while queue:
 
     for direction, adjTile in tileConnections[tile].items():
         # Loop through all possible rotations/flips until the matching one is found
-        # Performing these actions yields all possibilities after each one:
+        # Performing these actions yields all possible versions after each one:
         #   nothing, rot90CW, rot90CW, rot90CW, flipH, rot90CW, rot90CW, rot90CW 
         # TODO: Can be found directly instead of looping through all 8
         tries = 0
@@ -141,54 +132,45 @@ while queue:
 image = []
 tileSizeWithoutBorder = tileSize - 2
 for rowIndex in range(gridSize * tileSizeWithoutBorder):
-    row = []
-
     gridColumn = rowIndex // tileSizeWithoutBorder
     tileRow = rowIndex % tileSizeWithoutBorder
 
+    row = []
     for i in range(gridSize):
         row += tiles[grid[gridColumn][i]].imageWithoutBorders[tileRow]
     image.append(row)
 
+# Print matrix of tile IDs
 # for i in range(len(grid)): print(grid[i])
 
+# Print final image
 # print("\n".join("".join(row) for row in image))
 
-# Merge the tiles row by row (pretty version, with tile IDs and separators)
-# prettyImage = []
+# Print image but pretty (with borders, tile IDs and separators)
 # for rowIndex in range(gridSize * tileSize):
-
 #     gridColumn = rowIndex // tileSize
 #     tileRow = rowIndex % tileSize
-
 #     if tileRow == 0:
-#         prettyImage.append("-" * gridSize * (tileSize+1))
-#         tileIDs = [grid[gridColumn][k] for k in range(gridSize)]
-#         prettyImage.append("   " + "       ".join([str(s) for s in tileIDs]))
-
-#     row = []
-#     for i in range(gridSize):
-#         row += tiles[grid[gridColumn][i]].image[tileRow] + ["|"]
-#     prettyImage.append(row)
-
-# print("\n".join("".join(row) for row in prettyImage))
+#         print("-" * gridSize * (tileSize+1))
+#         tileIDs = [grid[gridColumn][i] for i in range(gridSize)]
+#         print("   " + "       ".join([str(s) for s in tileIDs]))
+#     print("|".join("".join(tiles[grid[gridColumn][i]].image[tileRow]) for i in range(gridSize)))
 
 monster = ["                  # ",
            "#    ##    ##    ###",
            " #  #  #  #  #  #   "]
-
 monsterRoughness = sum(row.count("#") for row in monster)
 
-# Assume a # can be part of more than one monster
 # Assume the correct rotation/flip will be the only one with any monsters
-
 monsters = 0
 tries = 0
+# Loop through all possible rotations/flips until a monster is found
 while monsters == 0:
-    # Scan the image, looking for monsters
     for i in range(len(image) - len(monster) + 1):
         for j in range(len(image[0]) - len(monster[0]) + 1):
             roughness = 0
+            # Assume a # can be part of more than one monster (although
+            # I'm pretty sure this is not the case in any of the actual inputs)
             for mi in range(len(monster)):
                 for mj in range(len(monster[0])):
                     if monster[mi][mj] == "#" and image[i+mi][j+mj] == "#":
@@ -196,8 +178,7 @@ while monsters == 0:
 
             monsters += int(roughness == monsterRoughness)
 
-    # Loop through all possible rotations/flips until a monster is found
-    # Performing these actions yields all possibilities after each one:
+    # Performing these actions yields all possible versions after each one:
     #   nothing, rot90CW, rot90CW, rot90CW, flipH, rot90CW, rot90CW, rot90CW 
     if tries == 3:
         image = flipH(image)
