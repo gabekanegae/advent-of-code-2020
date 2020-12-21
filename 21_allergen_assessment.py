299709c1e0cc460629faf9a8c2f7466eab41346e
Gabriel Kanegae <gabekanegae@gmail.com>
2020-12-21 07:22:51 -0300

    Day 21 - Simplify code with set methods

--- 21_allergen_assessment.py
+++ 21_allergen_assessment.py
@@ -34,47 +34,35 @@ for ingredients, allergies in foods:
 # Narrow down the possibilities by taking intersections
 # of ingredients of recipes that cause each allergy
 for allergy in possibleCauses:
-    ingredientLists = possibleCauses[allergy]
+    possibleCauses[allergy] = set.intersection(*possibleCauses[allergy])
 
-    intersection = set(ingredientLists[0])
-    for ingredientList in ingredientLists[1:]:
-        intersection &= ingredientList
-
-    possibleCauses[allergy] = intersection
-
-mayCauseAllergy = set()
-for ingredients in possibleCauses.values():
-    mayCauseAllergy |= ingredients
-
+mayCauseAllergy = set.union(*possibleCauses.values())
 cantCauseAllergy = set(allIngredients) - mayCauseAllergy
 
 p1 = sum(ingredients in cantCauseAllergy for ingredients in allIngredients)
 print("Part 1: {}".format(p1))
 
-# Sort by ascending amount of possible causes
 possibleCausesList = list(possibleCauses.items())
-possibleCausesList.sort(key=lambda x: len(x[1]))
 
-allergyCauses = {allergy: None for allergy in possibleCauses}
-
-# Assume len(possibleCausesList[0]) == 1, and the correct answer
-# can be found by cascading the correct answers from before
+# Assume the correct answer can be found by cascading
+# the correct answers from before
+allergyCauses = dict()
 for i in range(len(possibleCausesList)):
     # Always get the ingredient with the smallest amount of possibilities
     if len(possibleCausesList[0][1]) != 1:
         possibleCausesList.sort(key=lambda x: len(x[1]))
+
+    # Assume len(possibleCausesList[0][1]) == 1, i.e. there's only one answer
     allergy, possible = possibleCausesList[0]
 
-    # Found correct answer
-    if len(possible) == 1:
-        cause = possible.pop()
+    cause = possible.pop()
 
-        allergyCauses[allergy] = cause
+    allergyCauses[allergy] = cause
 
-        # Remove determined cause from all other possibilities
-        possibleCausesList.pop(0)
-        for j in range(len(possibleCausesList)):
-            possibleCausesList[j][1].discard(cause)
+    # Remove determined cause from all other possibilities
+    possibleCausesList.pop(0)
+    for j in range(len(possibleCausesList)):
+        possibleCausesList[j][1].discard(cause)
 
 allergyCausesList = list(allergyCauses.items())
 allergyCausesList.sort(key=lambda x: x[0])
