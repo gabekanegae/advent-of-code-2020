925ee146de693d3a9ad4ef9b19ba8354f88049c2
Gabriel Kanegae <gabekanegae@gmail.com>
2020-12-24 03:58:02 -0300

    Day 17 - Store only active cubes

--- 17_conway_cubes.py
+++ 17_conway_cubes.py
@@ -11,41 +11,35 @@ def conwayCubes(rawGrid, dimensions, cycles=6):
     nullMove = set([tuple([0]*dimensions)])
     moves = list(allMoves - nullMove)
 
-    grid = dict()
+    active = set()
     for x in range(len(rawGrid)):
         for y in range(len(rawGrid[0])):
-            key = tuple([x, y] + [0]*(dimensions - 2))
-            grid[key] = rawGrid[x][y]
+            if rawGrid[x][y] == "#":
+                pos = tuple([x, y] + [0]*(dimensions - 2))
+                active.add(pos)
 
     for cycle in range(cycles):
-        # Create one more layer on grid
-        for key in list(grid.keys()):
+        toBeUpdated = set(active)
+        for pos in active:
             for delta in moves:
-                deltaKey = tuple(k+d for k, d in zip(key, delta))
-                if deltaKey not in grid:
-                    grid[deltaKey] = "."
+                neighbor = tuple(k+d for k, d in zip(pos, delta))
+                toBeUpdated.add(neighbor)
 
-        # Deepcopy grid
-        newGrid = dict()
-        for key, cube in grid.items():
-            newGrid[key] = cube
-
-        # Update grid
-        for key in grid:
+        newActive = set(active)
+        for pos in toBeUpdated:
             neighbors = 0
             for delta in moves:
-                deltaKey = tuple(k+d for k, d in zip(key, delta))
-                if deltaKey in grid and grid[deltaKey] == "#":
-                    neighbors += 1
+                neighbor = tuple(k+d for k, d in zip(pos, delta))
+                neighbors += int(neighbor in active)
 
-            if grid[key] == "#" and neighbors not in [2, 3]:
-                newGrid[key] = "."
-            elif grid[key] == "." and neighbors == 3:
-                newGrid[key] = "#"
+            if pos in active and neighbors not in [2, 3]:
+                newActive.remove(pos)
+            elif pos not in active and neighbors == 3:
+                newActive.add(pos)
 
-        grid = newGrid
+        active = newActive
 
-    return list(grid.values()).count("#")
+    return len(active)
 
 ################################
 
